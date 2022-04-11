3c796a03b99a15caebd86cd8dfaf9ccca0abc014
vivekagarwal14 <86423179+vivekagarwal14@users.noreply.github.com>
2022-04-11 12:58:53 -0400

    update

--- server/query.js
+++ server/query.js
@@ -27,7 +27,37 @@ const createUser = (request, response) => {
   })
 }
 
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { name, email } = request.body
+
+  pool.query(
+    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
+}
+
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
+}
+
 module.exports = {
   getUsers,
+  getUserById,
   createUser,
+  updateUser,
+  deleteUser,
 }
